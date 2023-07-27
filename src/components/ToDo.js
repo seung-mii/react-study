f79675fd1b7a5335f7a687402570cb958d97d439
withsme <tmdal9953@gmail.com>
2023-07-27 14:28:56 +0900

    feat : Redux를 활용한 To-Do List 삭제 기능

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function ToDo({ text }) {
+function ToDo({ text, onBtnClick }) {
   return (
-    <li> {text} </li>
+    <li>
+      {text} <button onClick={onBtnClick}>DEL</button>
+    </li>
   );
 }
 
-export default ToDo;
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+    // 원래 store에 저장되어 있던 state를 삭제해야 하므로 ownProps를 활용
+  };
+}
+
+// mapStateToProps는 값을 읽어오는 것 뿐이므로 삭제하기 위해선 필요없어서 null 처리
+export default connect(null, mapDispatchToProps)(ToDo);
